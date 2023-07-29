82971966a6929064d48c515c0bb47e0f87ac3d9a
leenux <leenux@163.com>
2023-07-29 17:57:48 +0800

    Update AutoExpEnable.lua remove useless chat msg

--- AutoExpEnable.lua
+++ AutoExpEnable.lua
@@ -6,7 +6,7 @@ EXP:RegisterEvent("CHAT_MSG_SYSTEM")
 EXP:SetScript("OnEvent", function()
 	if event == "CHAT_MSG_SYSTEM" then
 		if ("XP gain is now OFF" == arg1) or ("XP gain is OFF" == arg1) then
-			DEFAULT_CHAT_FRAME:AddMessage("Your exp disabled and you can .xp on to enable=="..arg1)
+			DEFAULT_CHAT_FRAME:AddMessage("Your exp disabled and you can .xp on to enable")
 			UIErrorsFrame:AddMessage("Your exp disabled and you can .xp on to enable", 1.0, 0.0, 0.0, 53, 5)
 		end
     end
