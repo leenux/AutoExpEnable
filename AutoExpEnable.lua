d4a134d62aae9ceab4e42152a7803a1556bb1015
leenux <leenux@163.com>
2023-08-12 17:46:02 +0800

    Add features: Server restart notify

--- AutoExpEnable.lua
+++ AutoExpEnable.lua
@@ -6,8 +6,12 @@ EXP:RegisterEvent("PLAYER_LOGIN")
 EXP:SetScript("OnEvent", function()
     if event == "CHAT_MSG_SYSTEM" then
         if ("XP gain is now OFF" == arg1) or ("XP gain is OFF" == arg1) then
-            DEFAULT_CHAT_FRAME:AddMessage("Your exp disabled and you can .xp on to enable")
-            UIErrorsFrame:AddMessage("Your exp disabled and you can .xp on to enable", 1.0, 0.0, 0.0, 53, 5)
+            DEFAULT_CHAT_FRAME:AddMessage("Now exp gain OFF can .xp on to ON")
+            UIErrorsFrame:AddMessage("Now exp gain OFF can .xp on to ON", 1.0, 0.0, 0.0, 53, 5)
+        end
+        if(string.find(arg1,"erver ") ~= nil and string.find(arg1," restart") ~= nil) then
+            DEFAULT_CHAT_FRAME:AddMessage(arg1)
+            UIErrorsFrame:AddMessage(arg1, 1.0, 0.0, 0.0, 53, 5)
         end
     end
     if event == "PLAYER_LOGIN" then
